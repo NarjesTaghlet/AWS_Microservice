53fc3a24cb484eaa0f5ebb31ea9e16e42a117e61
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2025-07-30 08:59:20 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',  // URL of your Angular app (adjust as needed)
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],  // Allowed methods
+    origin: ['http://localhost:4200','https://davbjddcsuel.cloudfront.net'], // URL of your Angular app (adjust as needed)
+    methods: ['GET', 'POST', 'PATCH', 'DELETE','OPTIONS'],  // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed headers
   });
 
   // Listen for HTTP requests on port 3000
-  await app.listen(3002);
+ // await app.listen(3002);
+  await app.listen(3034);
+
 }
 bootstrap();
